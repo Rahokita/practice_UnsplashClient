43b821289d5d587c220da34489702aa6f6a43c23
rahokita <rehokita7352@yahoo.co.jp>
2024-02-24 22:54:18 +0900

    No.94 Update PhotoDetailContent (1/2)

--- app/src/main/java/com/example/unsplashclient/presentation/photo_detail/PhotoDetailScreen.kt
+++ app/src/main/java/com/example/unsplashclient/presentation/photo_detail/PhotoDetailScreen.kt
@@ -1,16 +1,20 @@
 package com.example.unsplashclient.presentation.photo_detail
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
+import com.example.unsplashclient.domain.model.PhotoDetail
 
 @Composable
 fun PhotoDetailScreen(
@@ -40,6 +44,33 @@ fun PhotoDetailScreen(
                 }
 
             }
+        }
+    }
+}
+
+@Composable
+fun PhotoDetailContent(photoDetail:PhotoDetail){
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+        Box(modifier = Modifier.heightIn(min = 200.dp)){
+            var isLoadingImage by remember { mutableStateOf(true) }
+            if (isLoadingImage){
+                CircularProgressIndicator(
+                    modifier = Modifier.align(Alignment.Center),
+                )
+            }
+            AsyncImage(
+                model = photoDetail.imageUrl,
+                contentDescription = photoDetail.description,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(
+                        RoundedCornerShape(
+                            topStartPercent = 0, topEndPercent = 0,
+                            bottomStartPercent = 5, bottomEndPercent = 5,
+                        )
+                    ),
+                onSuccess = { isLoadingImage = false},
+            )
         }
     }
 }
